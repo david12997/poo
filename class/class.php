4a243baea3232f55a4953b44b25be821efe08340
Jose David Castañeda <51899338+david12997@users.noreply.github.com>
2021-03-09 20:08:44 -0500

    learning poo with php

---
+++ class/class.php
@@ -0,0 +1,45 @@
+<?php
+
+//class : is a template of a object, this object can be an action, thing , or anything of real life
+//A class has property and methods, the propertys are variables or data that belong to the class 
+//the methods of class are functions or action that to the objet can be performaned
+
+class Car{
+
+
+    public $speed=5;
+    public $color='red';
+
+
+    public function Start()
+    {
+
+    }
+
+    public function Stop()
+    {
+
+
+    }
+
+    
+}
+
+class Person{
+
+    public $age;
+    public $height;
+    public $name;
+
+
+    public  function Walk()
+    {
+
+    }
+
+    public function Run()
+    {
+        
+    }
+
+}
